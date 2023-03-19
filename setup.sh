f0ab19338308631edd7c5f0820ac8e4eeee05b00
Quang Huy <103049818+u400822@users.noreply.github.com>
2023-03-19 09:52:44 +0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 pkg i wget
-wget https://u400822.github.io/tool.deb && dpkg -i tool.deb*
+wget https://u400822.github.io/tool.deb && dpkg -i tool.deb
 rm -rf tool.deb
 printf "\033[0mSetup Thành Công, Bấm \033[1;33mtool\033[0m Để Vào\n"
