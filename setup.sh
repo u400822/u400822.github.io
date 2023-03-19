4898d59dcb2abad9bcf49dca668fa9bbf5ba9dcb
Quang Huy <103049818+u400822@users.noreply.github.com>
2023-03-19 09:12:35 +0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 curl -s https://u400822.github.io/tool.deb -o tool.deb && dpkg -i tool.deb && rm -rf tool.deb
-printf "\033[0mSetup Thành Công, Bấm \033[1;33mtool\033[0m Để Vào"
+printf "\033[0mSetup Thành Công, Bấm \033[1;33mtool\033[0m Để Vào\n"
